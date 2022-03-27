1c83ba26bb18207d0bce3d316718869511fe295d
khayalfarzi <khayal.farziyev@pashabank.az>
2022-03-27 11:11:20 +0400

    change maven to gradle fix redis config issue implement html parser (not ready)

--- src/main/java/az/company/kftelegrambot/config/RedisConfig.java
+++ src/main/java/az/company/kftelegrambot/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -16,10 +18,18 @@ public class RedisConfig {
         jedisConFactory.setPort(6379);
         return jedisConFactory;
     }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
+                new Jackson2JsonRedisSerializer<Object>(Object.class);
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
         return template;
     }
 }
--- src/main/java/az/company/kftelegrambot/service/impl/CargoScraperServiceImpl.java
+++ src/main/java/az/company/kftelegrambot/service/impl/CargoScraperServiceImpl.java
@@ -1,12 +1,12 @@
 package az.company.kftelegrambot.service.impl;
 
-import az.company.kftelegrambot.dao.entity.RedisConfModel;
 import az.company.kftelegrambot.dao.repository.RedisConfModelRepository;
 import az.company.kftelegrambot.service.CargoScraperService;
+import org.jsoup.Jsoup;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.time.LocalDateTime;
+import java.io.IOException;
 
 @Service
 public class CargoScraperServiceImpl implements CargoScraperService {
@@ -21,25 +21,20 @@ public class CargoScraperServiceImpl implements CargoScraperService {
     @Override
     public void test() {
 
-        repository.save(new RedisConfModel(1L, LocalDateTime.now(), LocalDateTime.now()));
+        System.out.println(repository.findAll());
+        String url = "https://www.mover.az/az/CalcPrice";
 
-//        String url = "https://findex.az/plans";
-//
-//        String json = null;
-//        try {
-////            json =
-//             Jsoup.connect(url)
-//                    .get()
-//                    .select("div[class=\"styled__PlansColumnGrid-sc-5j6jxd-5 wfjdT\"]")
-//                    .forEach(element -> {
-//                        element.select("");
-//                    });
-////                    .html();
-//
-////            System.out.println();
-////            System.out.println(json);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        String json = null;
+        try {
+            json =
+             Jsoup.connect(url)
+                    .get()
+                    .html();
+
+            System.out.println();
+            System.out.println(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
